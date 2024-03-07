e2eba46b667e28e79bab996b9ad577dc3591c38f
Eric Wood <eric@ericwood.org>
2024-03-06 18:47:17 -0700

    do the switching thing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
-#![feature(generic_const_exprs)]
 
-use defmt::info;
 use embassy_executor::Spawner;
 use embassy_futures::select::{select, Either};
-use embassy_stm32::{bind_interrupts, exti::Channel, gpio::Pin, i2c, peripherals};
+use embassy_stm32::{
+    bind_interrupts,
+    exti::Channel,
+    gpio::{Level, Output, Pin},
+    i2c, peripherals,
+};
 use embassy_time::Duration;
 use {defmt_rtt as _, panic_probe as _};
-// use stm32f0xx_hal::
 
 mod debounced_button;
 use debounced_button::DebouncedButton;
@@ -31,22 +33,45 @@ bind_interrupts!(struct Irqs {
 async fn main(_spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
 
-    let persistence = Persistence::<State>::new(p.I2C1, p.PA9, p.PA10);
+    let mut persistence = Persistence::<State>::new(p.I2C1, p.PA9, p.PA10);
 
-    let input_1 = ToggleWithMomentary::new(
+    let mut input_1 = ToggleWithMomentary::new(
         persistence.state.switch_1,
         p.PA0.degrade(),
         DEBOUNCE_THRESHOLD,
         HOLD_THRESHOLD,
         p.EXTI0.degrade(),
     );
-    let input_1 = ToggleWithMomentary::new(
+    let mut output_1 = Output::new(
+        p.PA2,
+        Level::from(persistence.state.switch_1),
+        embassy_stm32::gpio::Speed::Low,
+    );
+
+    let mut input_2 = ToggleWithMomentary::new(
         persistence.state.switch_2,
         p.PA1.degrade(),
         DEBOUNCE_THRESHOLD,
         HOLD_THRESHOLD,
         p.EXTI1.degrade(),
     );
+    let mut output_2 = Output::new(
+        p.PA3,
+        Level::from(persistence.state.switch_2),
+        embassy_stm32::gpio::Speed::Low,
+    );
 
-    loop {}
+    loop {
+        let input_1_future = input_1.wait_for_state_change();
+        let input_2_future = input_2.wait_for_state_change();
+        match select(input_1_future, input_2_future).await {
+            Either::First(_) => {}
+            Either::Second(_) => {}
+        }
+
+        let state = State::new(input_1.is_enabled, input_2.is_enabled);
+        output_1.set_level(Level::from(state.switch_1));
+        output_2.set_level(Level::from(state.switch_2));
+        persistence.update(state).unwrap();
+    }
 }
--- src/state.rs
+++ src/state.rs
@@ -5,6 +5,12 @@ pub struct State {
     pub switch_2: bool,
 }
 
+impl State {
+    pub fn new(switch_1: bool, switch_2: bool) -> Self {
+        State { switch_1, switch_2 }
+    }
+}
+
 impl Persistable for State {
     fn from_u8(input: u8) -> Self {
         let switch_1 = (input & (1 << 0)) > 0;
