227727979d3688eda9cdce823b0c0e63f44aba14
Eric Wood <eric@ericwood.org>
2024-03-06 17:44:22 -0700

    Generic persistence

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
+#![feature(generic_const_exprs)]
 
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_futures::select::{select, Either};
-use embassy_stm32::{
-    bind_interrupts,
-    dma::NoDma,
-    exti::{Channel, ExtiInput},
-    gpio::{Input, Level, Output, Pin, Pull, Speed},
-    i2c::{self, I2c},
-    peripherals,
-};
+use embassy_stm32::{bind_interrupts, exti::Channel, gpio::Pin, i2c, peripherals};
 use embassy_time::Duration;
 use {defmt_rtt as _, panic_probe as _};
 // use stm32f0xx_hal::
@@ -23,6 +17,8 @@ mod toggle_with_momentary;
 use toggle_with_momentary::ToggleWithMomentary;
 mod persistence;
 use persistence::Persistence;
+mod state;
+use state::State;
 
 static HOLD_THRESHOLD: Duration = Duration::from_millis(700);
 static DEBOUNCE_THRESHOLD: Duration = Duration::from_millis(7);
@@ -35,7 +31,7 @@ bind_interrupts!(struct Irqs {
 async fn main(_spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
 
-    let persistence = Persistence::new(p.I2C1, p.PA9, p.PA10);
+    let persistence = Persistence::<State>::new(p.I2C1, p.PA9, p.PA10);
 
     let input_1 = ToggleWithMomentary::new(
         false,
--- src/persistence.rs
+++ src/persistence.rs
@@ -1,3 +1,5 @@
+use core::marker::PhantomData;
+
 use crate::Irqs;
 use eeprom24x::{
     addr_size::OneByte, page_size::B8, unique_serial::No, Eeprom24x, SlaveAddr, Storage,
@@ -10,11 +12,25 @@ use embassy_stm32::{
 };
 use embassy_time::Delay;
 
-pub struct Persistence<'a> {
-    storage: Storage<I2c<'a, I2C1>, B8, OneByte, No, Delay>,
+static MAX_ADDRESS: u32 = 100; // TODO
+
+pub trait Persistable {
+    fn from(input: u8) -> Self;
+    fn into(&self) -> u8;
 }
 
-impl<'a> Persistence<'a> {
+pub struct Persistence<'a, T>
+where
+    T: Persistable,
+{
+    storage: Storage<I2c<'a, I2C1>, B8, OneByte, No, Delay>,
+    state: T,
+}
+
+impl<'a, T> Persistence<'a, T>
+where
+    T: Persistable,
+{
     pub fn new(i2c: I2C1, scl_pin: impl SclPin<I2C1>, sda_pin: impl SdaPin<I2C1>) -> Self {
         let i2c = I2c::new(
             i2c,
@@ -28,15 +44,23 @@ impl<'a> Persistence<'a> {
         );
 
         let address = SlaveAddr::default();
-        let eeprom: Eeprom24x<
-            I2c<'_, I2C1>,
-            eeprom24x::page_size::B8,
-            eeprom24x::addr_size::OneByte,
-            eeprom24x::unique_serial::No,
-        > = Eeprom24x::new_24x02(i2c, address);
+        let eeprom = Eeprom24x::new_24x02(i2c, address);
 
         let storage = Storage::new(eeprom, Delay);
+        // for address in 0..MAX_ADDRESS {
+        //     // let value = storage.eeprom.read_data(address, U);
+        //     // check if the most significant bit is 0
+        //     let mut data = [0xFF; U::BYTES];
+        //     storage.eeprom.read_data(address, &data);
+        //     if value >> (U::BITS - 1) == U::zero() {
+        //         self.value = value;
+        //         self.current_address = address;
+        //         break;
+        //     }
+        // }
 
-        Persistence { storage }
+        let state = T::from(0);
+
+        Persistence { storage, state }
     }
 }
